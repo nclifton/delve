3080dec4973160f9f2167fba7f7bfe1ab2c24f25
Mike Bissett <mike@burstsms.com>
2020-12-09 16:30:11 +1100

    176045815 forestadmin service (#58)
    * [#176045815] Setup forestadmin for develop
    
    * [#176045815] Created the infra for staging and production
    
    * Fixed up infra files

---
+++ backend/forestadmin/routes/account.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const { PermissionMiddlewareCreator } = require('forest-express-sequelize');
+const { account } = require('../models');
+
+const router = express.Router();
+const permissionMiddlewareCreator = new PermissionMiddlewareCreator('account');
+
+// This file contains the logic of every route in Forest Admin for the collection account:
+// - Native routes are already generated but can be extended/overriden - Learn how to extend a route here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/extend-a-route
+// - Smart action routes will need to be added as you create new Smart Actions - Learn how to create a Smart Action here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/actions/create-and-manage-smart-actions
+
+// Create a Account
+router.post('/account', permissionMiddlewareCreator.create(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/default-routes#create-a-record
+  next();
+});
+
+// Update a Account
+router.put('/account/:recordId', permissionMiddlewareCreator.update(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/default-routes#update-a-record
+  next();
+});
+
+// Delete a Account
+router.delete('/account/:recordId', permissionMiddlewareCreator.delete(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/default-routes#delete-a-record
+  next();
+});
+
+// Get a list of Accounts
+router.get('/account', permissionMiddlewareCreator.list(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/default-routes#get-a-list-of-records
+  next();
+});
+
+// Get a number of Accounts
+router.get('/account/count', permissionMiddlewareCreator.list(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/default-routes#get-a-number-of-records
+  next();
+});
+
+// Get a Account
+router.get('/account/:recordId', permissionMiddlewareCreator.details(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/default-routes#get-a-record
+  next();
+});
+
+// Export a list of Accounts
+router.get('/account.csv', permissionMiddlewareCreator.export(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/default-routes#export-a-list-of-records
+  next();
+});
+
+// Delete a list of Accounts
+router.delete('/account', permissionMiddlewareCreator.delete(), (request, response, next) => {
+  // Learn what this route does here: https://docs.forestadmin.com/documentation/v/v6/reference-guide/routes/default-routes#delete-a-list-of-records
+  next();
+});
+
+module.exports = router;
