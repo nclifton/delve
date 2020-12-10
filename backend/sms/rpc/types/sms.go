1314bcdfeb7c8388a13cfb4c0266d0fb002ce636
Mike Bissett <mike@burstsms.com>
2020-12-10 15:31:15 +1100

    176084395 fix json formatting on api and webhook responses (#60)
    * [#176084395] Changed the sms response json
    
    * [#176084395] Checked and fixed up webhook json format

--- backend/sms/rpc/types/sms.go
+++ backend/sms/rpc/types/sms.go
@@ -3,20 +3,20 @@ package types
 import "time"
 
 type SMS struct {
-	ID         string
-	AccountID  string
-	MessageID  string
-	Recipient  string
-	Sender     string
-	Country    string
-	MessageRef string
-	Message    string
-	Status     string
-	SMSCount   int
-	GSM        bool
-	CreatedAt  time.Time
-	UpdatedAt  time.Time
-	TrackLinks bool
+	ID         string    `json:"id"`
+	AccountID  string    `json:"account_id"`
+	MessageID  string    `json:"message_id"`
+	Recipient  string    `json:"recipient"`
+	Sender     string    `json:"sender"`
+	Country    string    `json:"country"`
+	MessageRef string    `json:"message_ref"`
+	Message    string    `json:"message"`
+	Status     string    `json:"status"`
+	SMSCount   int       `json:"sms_count"`
+	GSM        bool      `json:"gsm"`
+	CreatedAt  time.Time `json:"created_at"`
+	UpdatedAt  time.Time `json:"updated_at"`
+	TrackLinks bool      `json:"track_links"`
 }
 
 type SendParams struct {
--- backend/webhook/rpc/types/track_link.go
+++ backend/webhook/rpc/types/track_link.go
@@ -3,9 +3,9 @@ package types
 import "time"
 
 type PublishLinkHitParams struct {
-	URL           string
-	Hits          int
-	Timestamp     time.Time
-	SourceMessage *SourceMessage
-	AccountID     string
+	URL           string         `json:"url"`
+	Hits          int            `json:"hits"`
+	Timestamp     time.Time      `json:"timestamp"`
+	SourceMessage *SourceMessage `json:"source_message,omitempty"`
+	AccountID     string         `json:"account_id"`
 }
