9cb4eeaf32870dcf0f66c0f132e35270bdc9ab33
Thomas <57207652+thomasburst@users.noreply.github.com>
2021-01-28 19:26:20 +1100

    Stopped db data from being deleted (#124)

--- ops/create_cluster/modules/aurora_postgresql/main.tf
+++ ops/create_cluster/modules/aurora_postgresql/main.tf
@@ -13,10 +13,13 @@ resource "aws_rds_cluster_instance" "cluster_instances" {
   count                = 3
   identifier           = "${terraform.workspace}-${count.index}"
   cluster_identifier   = aws_rds_cluster.postgresql.id
-  db_subnet_group_name = aws_db_subnet_group.dbsubnet.id
-  instance_class       = "db.t3.medium"
+
   engine               = "aurora-postgresql"
   engine_version       = "11.7"
+
+  db_subnet_group_name = aws_db_subnet_group.dbsubnet.id
+  instance_class       = "db.t3.medium"
+  
   tags = {
     "managed-by"   = "terraform"
     "cluster-name" = terraform.workspace
@@ -25,16 +28,21 @@ resource "aws_rds_cluster_instance" "cluster_instances" {
 
 resource "aws_rds_cluster" "postgresql" {
   cluster_identifier        = terraform.workspace
+
   engine                    = "aurora-postgresql"
   engine_version            = "11.7"
-  availability_zones        = ["ap-southeast-2b", "ap-southeast-2a", "ap-southeast-2c"]
+ 
   database_name             = "sendsei"
   master_username           = "foo"
   master_password           = "barbut8chars"
-  skip_final_snapshot       = true
-  final_snapshot_identifier = "sendseifinalsnapshot"
+
+  deletion_protection       = true
+  final_snapshot_identifier = "sendsei-final-snapshot"
+
   db_subnet_group_name      = aws_db_subnet_group.dbsubnet.id
+  availability_zones        = ["ap-southeast-2a", "ap-southeast-2b", "ap-southeast-2c"]
   vpc_security_group_ids    = [data.aws_security_group.clustersg.id]
+
   tags = {
     "managed-by"   = "terraform"
     "cluster-name" = terraform.workspace
