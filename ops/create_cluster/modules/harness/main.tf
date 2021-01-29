21d6a484941298693843364e87adcbac4d382694
Thomas <57207652+thomasburst@users.noreply.github.com>
2021-01-29 13:45:54 +1100

    Add terraform flag to stop harness being deployed (#126)
    * Updated inline with sendsei
    
    * Updated inline with sendsei

--- ops/create_cluster/modules/harness/main.tf
+++ ops/create_cluster/modules/harness/main.tf
@@ -1,23 +1,14 @@
-resource "kubernetes_namespace" "harness" {
-  metadata {
-    name = "harness"
-    labels = {
-      "managed-by"   = "terraform"
-      "cluster-name" = terraform.workspace
-    }
-  }
-}
-
 resource "helm_release" "harness" {
+  # Only deploy to staging and production environments
+  # https://stackoverflow.com/a/58193941
+  count = (length(regexall(".*staging.*", terraform.workspace)) > 0 || length(regexall(".*production.*", terraform.workspace)) > 0) ? 1 : 0
+
   name       = "harness"
   repository = "https://app.harness.io/storage/harness-download/harness-helm-charts/"
   chart      = "harness-delegate"
-  namespace  = "harness"
   version    = "1.0.2"
 
   values = [
     templatefile("${path.module}/values.tmpl.yaml", { environment_name : terraform.workspace })
   ]
-
-  depends_on = [kubernetes_namespace.harness]
 }
