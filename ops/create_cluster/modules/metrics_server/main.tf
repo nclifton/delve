a37783ca413eadb0cd2b99688a96db7a2edf4bb0
Thomas <57207652+thomasburst@users.noreply.github.com>
2021-02-01 10:31:15 +1100

    Updated metrics-server version (#131)

--- ops/create_cluster/modules/metrics_server/main.tf
+++ ops/create_cluster/modules/metrics_server/main.tf
@@ -1,13 +1,18 @@
 resource "helm_release" "metric-server" {
-  name       = "metric-server"
+  name       = "metrics-server"
   repository = "https://charts.bitnami.com/bitnami" 
   chart      = "metrics-server"
-  version    = "5.0.1"
+  version    = "5.4.0"
 
   namespace = "kube-system"
 
   set {
     name  = "apiService.create"
+    value = "true"
+  }
+
+  set {
+    name  = "rbac.create"
     value = "true"
   }
 }
