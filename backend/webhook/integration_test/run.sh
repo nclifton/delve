9b356ab406aadaed2b1b981af764da100a7b654e
Neil Clifton <neil@burstsms.com>
2021-02-04 15:18:43 +1100

    176777460-Failing-domain-integration-tests-not-being-detected-by-pipeline (#143)
    * and together the two integration script run commands
    
    * drop sender integration test
    
    * separate webhook from sender integration tests in workflow
    
    * not using shell scripts for the integration tests
    
    * prob with run
    
    * typo
    
    * attempt to exit script with appropriate exit code
    
    * recombine integration tests
    
    * some odd syntax is required
    
    * fixed integration test run shell scripts
    
    * restored fix to the webhook integration tests for health check postgres not available test

--- backend/webhook/integration_test/run.sh
+++ backend/webhook/integration_test/run.sh
@@ -3,14 +3,9 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 pushd $DIR &> /dev/null
 
-export $(grep -v '^#' ../rpc/infra/values/dev-values.env | xargs)
-
-export MIGRATION_ROOT="file://../migration/sql"
-export TEST_FIXTURE_POSTGRES_USER=gnomock
-export TEST_FIXTURE_POSTGRES_USER_PASSWORD=gnomick
-export TEST_FIXTURE_RABBITMQ_USER=gnomock
-export TEST_FIXTURE_RABBITMQ_USER_PASSWORD=gnomick
-
 go test -timeout 300s -tags integration -v -run ^Test_.*$
+status=$?
 
 popd &> /dev/null
+
+exit $status
